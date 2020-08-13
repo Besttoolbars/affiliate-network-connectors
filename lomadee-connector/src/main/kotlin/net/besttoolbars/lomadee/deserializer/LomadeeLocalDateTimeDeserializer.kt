a7f1ad0e3f96355025fac79271fd2f39df3ed26b
Denis Dyachkov <denis.dyachkov@softomate.com>
2020-08-14 00:52:43 +0700

    add datetime deserializers

---
+++ lomadee-connector/src/main/kotlin/net/besttoolbars/lomadee/deserializer/LomadeeLocalDateTimeDeserializer.kt
@@ -0,0 +1,24 @@
+package net.besttoolbars.lomadee.deserializer
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class LomadeeLocalDateTimeDeserializer : JsonDeserializer<LocalDateTime?>() {
+    override fun deserialize(p: JsonParser?, ctxt: DeserializationContext?): LocalDateTime? {
+        val result: String? = StringDeserializer.instance.deserialize(p, ctxt)
+        if (result.isNullOrBlank()) {
+            return null
+        }
+        return LocalDateTime.parse(result,
+            formatter
+        )
+    }
+
+    companion object {
+        private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    }
+}
