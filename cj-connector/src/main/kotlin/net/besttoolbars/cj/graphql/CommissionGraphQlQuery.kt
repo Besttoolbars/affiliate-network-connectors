a0d5ae18daf67351e9dbc3a7e8c71877a6fb139e
Denis Dyachkov <denis.dyachkov@softomate.com>
2021-04-06 11:55:43 +0700

    add commission query

--- cj-connector/src/main/kotlin/net/besttoolbars/cj/graphql/CommissionGraphQlQuery.kt
+++ cj-connector/src/main/kotlin/net/besttoolbars/cj/graphql/CommissionGraphQlQuery.kt
@@ -1,26 +1,31 @@
 package net.besttoolbars.cj.graphql
 
+import net.besttoolbars.connectors.shared.graphql.gqlParamsBuilder
 import java.time.LocalDateTime
 import java.time.ZoneId
-import java.time.format.DateTimeFormatter
+import java.time.ZonedDateTime
 
 object CommissionGraphQlQuery {
+    @Deprecated("Use publisherCommissions(query) instead", ReplaceWith("publisherCommissions(query)"))
     fun publisherCommissions(
         publisher: String,
         from: LocalDateTime,
         to: LocalDateTime = LocalDateTime.now(),
         sinceCommissionId: String? = null
     ): String {
-        val dateFromFormatted = from.atZone(ZoneId.of("Z")).format(DateTimeFormatter.ISO_DATE_TIME)
-        val dateToFormatted = to.atZone(ZoneId.of("Z")).format(DateTimeFormatter.ISO_DATE_TIME)
+        val query = Query(
+            forPublishers = setOf(publisher),
+            sinceCommissionId = sinceCommissionId,
+            sincePostingDate = from.atZone(ZoneId.of("Z")),
+            beforePostingDate = to.atZone(ZoneId.of("Z"))
+        )
+        return publisherCommissions(query)
+    }
+
+    fun publisherCommissions(query: Query): String {
         return """
-query{ 
-  publisherCommissions(
-    forPublishers: ["$publisher"],
-    sincePostingDate:"$dateFromFormatted",
-    beforePostingDate:"$dateToFormatted",
-    sinceCommissionId: $sinceCommissionId
-  ) {
+query { 
+  publisherCommissions(${query.toQueryString()}) {
     count 
     payloadComplete 
     maxCommissionId 
@@ -60,5 +65,44 @@ query{
   }
 }
         """.trimIndent()
+    }
+
+    class Query(
+        var forPublishers: Set<String>,
+        var sincePostingDate: ZonedDateTime? = null,
+        var beforePostingDate: ZonedDateTime? = null,
+        var sinceEventDate: ZonedDateTime? = null,
+        var beforeEventDate: ZonedDateTime? = null,
+        var sinceLockingDate: ZonedDateTime? = null,
+        var beforeLockingDate: ZonedDateTime? = null,
+        var sinceCommissionId: String? = null,
+        var commissionIds: Set<String>? = null,
+        var advertiserIds: Set<String>? = null,
+        var adIds: Set<String>? = null,
+        var websiteIds: Set<String>? = null,
+        var actionStatuses: Set<String>? = null,
+        var actionTypes: Set<String>? = null,
+        var lockingMethods: Set<String>? = null,
+        var validationStatuses: Set<String>? = null
+    ) {
+        fun toQueryString(): String =
+            gqlParamsBuilder {
+                "forPublishers" setNotNullable forPublishers
+                "sincePostingDate" set sincePostingDate
+                "beforePostingDate" set beforePostingDate
+                "sinceEventDate" set sinceEventDate
+                "beforeEventDate" set beforeEventDate
+                "sinceLockingDate" set sinceLockingDate
+                "beforeLockingDate" set beforeLockingDate
+                "sinceCommissionId" set sinceCommissionId
+                "commissionIds" set commissionIds
+                "advertiserIds" set advertiserIds
+                "adIds" set adIds
+                "websiteIds" set websiteIds
+                "actionStatuses" set actionStatuses
+                "actionTypes" set actionTypes
+                "lockingMethods" set lockingMethods
+                "validationStatuses" set validationStatuses
+            }
     }
 }
--- shared-retrofit-extensions/src/main/kotlin/net/besttoolbars/connectors/shared/graphql/GraphQLParamsBuilder.kt
+++ shared-retrofit-extensions/src/main/kotlin/net/besttoolbars/connectors/shared/graphql/GraphQLParamsBuilder.kt
@@ -1,23 +1,13 @@
 package net.besttoolbars.connectors.shared.graphql
 
-fun gqlParamsBuilder(f: GraphQLParamsBuilder.() -> Unit): String = GraphQLParamsBuilder(f).build()
+fun gqlParamsBuilder(builder: GraphQLParamsBuilder.() -> Unit): String = GraphQLParamsBuilder(builder).build()
 
-class GraphQLParamsBuilder(f: GraphQLParamsBuilder.() -> Unit) {
+class GraphQLParamsBuilder(builder: GraphQLParamsBuilder.() -> Unit) {
     private val params = mutableMapOf<String, Any?>()
 
     init {
-        f.invoke(this)
+        builder.invoke(this)
     }
-
-    private val Any.formatted: String
-        get() = when (this) {
-            is GraphQLParamsBuilder -> "{ ${this.build()} }"
-            is Iterable<*> -> this.filterNotNull().map { it.formatted }.toString()
-            is Array<*> -> this.asIterable().formatted
-            is GraphQLScalar -> this.string()
-            is String -> "\"$this\""
-            else -> toString()
-        }
 
     fun build(): String =
         params.map { (param, argument) -> "$param: ${argument?.formatted}" }
@@ -29,13 +19,24 @@ class GraphQLParamsBuilder(f: GraphQLParamsBuilder.() -> Unit) {
     }
 
     infix fun String.set(value: Any?) = set(value, nullable = true)
-    infix fun String.setNotNull(value: Any) = set(value, nullable = false)
+    infix fun String.setIfNotNull(value: Any?) = value?.let { set(value) }
+    infix fun String.setNotNullable(value: Any) = set(value, nullable = false)
     infix fun String.set(builder: GraphQLParamsBuilder.() -> Unit) = set(GraphQLParamsBuilder(builder))
 
     infix fun String.setIfNotEmpty(value: Collection<*>?) {
         if (value != null && value.isEmpty()) return
         this set value
     }
+
+    private val Any.formatted: String
+        get() = when (this) {
+            is GraphQLParamsBuilder -> "{ ${this.build()} }"
+            is Iterable<*> -> this.filterNotNull().map { it.formatted }.toString()
+            is Array<*> -> this.asIterable().formatted
+            is GraphQLScalar -> this.string()
+            is String -> "\"$this\""
+            else -> toString()
+        }
 }
 
 interface GraphQLScalar {
