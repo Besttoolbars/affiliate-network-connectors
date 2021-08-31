42e122d01d04c49e327cbe835407b73fd3a3c399
Denis Dyachkov <dda3004@gmail.com>
2021-08-31 16:00:47 +0700

    Update awin transaction response model
    Added information about products in the order basket.
    Added a model with click parameters.
    Updated field types.

--- awin-connector/src/main/kotlin/net/besttoolbars/awin/response/AwinTransaction.kt
+++ awin-connector/src/main/kotlin/net/besttoolbars/awin/response/AwinTransaction.kt
@@ -11,20 +11,21 @@ typealias AwinTransactionResponse = List<AwinTransaction>
  * @property lapseTime is the conversion time in seconds
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class AwinTransaction (
+data class AwinTransaction(
     val id: Long,
     val url: String,
     val advertiserId: Long,
     val publisherId: Long,
-    val commissionSharingPublisherId: Long,
-    val commissionSharingSelectedRatePublisherId: Long,
+    val commissionSharingPublisherId: Long?,
+    val commissionSharingSelectedRatePublisherId: Long?,
     val siteName: String,
     val commissionStatus: AwinTransactionStatus,
     val commissionAmount: Amount,
     val saleAmount: Amount,
+    val campaign: String? = null,
     val ipHash: String? = null,
     val customerCountry: String? = null,
-    val clickRefs: Map<String, String>? = null,
+    val clickRefs: ClickRefs? = null,
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     val clickDate: LocalDateTime,
     @JsonFormat(shape = JsonFormat.Shape.STRING)
@@ -32,48 +33,65 @@ data class AwinTransaction (
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     val validationDate: LocalDateTime? = null,
     val type: String? = null,
-
-    val declineReason: Any? = null,
-
+    val declineReason: String? = null,
     val voucherCodeUsed: Boolean = false,
     val voucherCode: String? = null,
     val lapseTime: Long = 0,
     val amended: Boolean = false,
-
-    val amendReason: Any? = null,
+    val amendReason: String? = null,
     val oldSaleAmount: Any? = null,
     val oldCommissionAmount: Any? = null,
-
     val clickDevice: String? = null,
     val transactionDevice: String? = null,
     val publisherUrl: String? = null,
     val advertiserCountry: String? = null,
     val orderRef: String? = null,
     val customParameters: List<CustomParameter>? = null,
-    val transactionParts: List<TransactionPart>? = null,
+    val transactionParts: List<Part>? = null,
     val paidToPublisher: Boolean = false,
     val paymentId: Long = 0,
     val transactionQueryId: Long = 0,
+    val originalSaleAmount: Any? = null,
+    val basketProducts: List<BasketProduct>? = null
+) {
+    data class Amount(
+        val amount: Double,
+        val currency: String
+    )
 
-    val originalSaleAmount: Any? = null
-)
+    data class CustomParameter(
+        val key: String,
+        val value: String
+    )
 
-data class Amount (
-    val amount: Double,
-    val currency: String
-)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    data class Part(
+        val commissionGroupId: Long,
+        val amount: Double,
+        val commissionAmount: Double,
+        val advertiserCost: Double?,
+        val commissionGroupCode: String,
+        val commissionGroupName: String
+    )
 
-data class CustomParameter (
-    val key: String,
-    val value: String
-)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    data class BasketProduct(
+        val quantity: Int,
+        val unitPrice: Double,
+        val skuCode: String?,
+        val category: String,
+        val productId: String?,
+        val productName: String,
+        val commissionGroupCode: String
+    )
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class TransactionPart (
-    val commissionGroupId: Long,
-    val amount: Double,
-    val commissionAmount: Double,
-    val advertiserCost: Double?,
-    val commissionGroupCode: String,
-    val commissionGroupName: String
-)
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    data class ClickRefs(
+        val clickRef: String? = null,
+        val clickRef2: String? = null,
+        val clickRef3: String? = null,
+        val clickRef4: String? = null,
+        val clickRef5: String? = null,
+        val clickRef6: String? = null
+    )
+}
